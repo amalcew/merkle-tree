0ad9aaaacddf3ca2a723372273eaf97e61bb9a92
Aleksander Malcew <amalcew@protonmail.com>
2021-12-08 10:55:20 +0100

    Started check_consistency() function, needs work

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ def build_tree(leaves):
             else:
                 tmp_array.append(parent1)
                 break
-            print("%s : %s" % (parent1.name, parent1.hash_value))
-            print("%s : %s" % (parent2.name, parent2.hash_value))
+            # print("%s : %s" % (parent1.name, parent1.hash_value))
+            # print("%s : %s" % (parent2.name, parent2.hash_value))
 
             concatenated_hash = parent1.hash_value + parent2.hash_value
             concatenated_name = parent1.name + "|" + parent2.name
             child_node = MerkleTreeNode(concatenated_hash, concatenated_name)
 
-            print("\tconcatenation of %s and %s : %s | hash : %s\n" % (parent1.name, parent2.name, concatenated_hash, child_node.hash_value))
+            # print("\tconcatenation of %s and %s : %s | hash : %s\n" % (parent1.name, parent2.name, concatenated_hash, child_node.hash_value))
 
             tmp_array.append(child_node)
         nodes = tmp_array
@@ -58,8 +58,27 @@ def check_inclusion(elem):
             return False
 
 
+def check_consistency(leaves1, leaves2):
+    # swap trees, if first given tree is not the smallest
+    if len(leaves1) > len(leaves2):
+        leaves1, leaves2 = leaves2, leaves1
+
+    # check if tree1 is subset of tree2
+    for i in range(len(leaves1)):
+        if leaves1[i] is not leaves2[i]:
+            return False
+
+    # create trees
+    tree1 = build_tree(leaves1)
+    tree1_dict = included
+    tree2 = build_tree(leaves2)
+    tree2_dict = included
+
+
+
 def main():
     arr = ['alice', 'bob', 'carol', 'david', 'eve']
+    arr2 = ['alice', 'bob', 'carol', 'david']
     """ Above array creates this tree:
         alice   bob     carol   david
             \   /           \   /
@@ -71,7 +90,7 @@ def main():
                          \                /
                        alice|bob|carol|david|eve
     """
-    print("Hash of %s: %s" % (arr, build_tree(arr)))
+    """print("Hash of %s: %s" % (arr, build_tree(arr)))
 
     test_1 = 'brian'
     test_1_hash = hashlib.sha256(test_1.encode('utf-8')).hexdigest()  # returns : f3fe5a51a2be8c6dc715028858fcba82ee021be7687e4f95b45086b8ffb1a23f
@@ -85,7 +104,9 @@ def main():
     print("\t- hash : %s : %s" % (test_2_hash, check_inclusion(test_2_hash)))  # returns : True
 
     # TODO: add argument parser to the main.py when all functions are completed
+    """
 
+    check_consistency(arr, arr2)
 
 if __name__ == '__main__':
     main()
