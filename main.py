69d66983e0e6f7936df53e2a1da8d35117efd7fb
Alexander Malcew <amalcew@protonmail.com>
2021-12-20 23:44:29 +0100

    Merkle Tree modified to reliable driver

--- main.py
+++ main.py
@@ -1,45 +1,14 @@
-from merkle_tree import MerkleTree, check_consistency
-import hashlib
+from merkle_tree import MerkleTree
 
 
 def main():
-    arr = ['alice', 'bob', 'carol', 'david', 'eve']
-    arr2 = ['alice', 'bob', 'carol', 'david']
-    """ Above array creates this tree:
-        alice   bob     carol   david
-            \   /           \   /
-          alice|bob       carol|david
-               \              /
-                 \           /
-             alice|bob|carol|david         eve
-                       \                   /
-                         \                /
-                       alice|bob|carol|david|eve
-    """
-    tree = MerkleTree(arr)
-    print("Final hash of %s: %s" % (tree.leaves, tree.get_final_hash()))
+    arr = ['alice', 'bob', 'carol', 'david', 'eve', 'fred', 'george', 'hans', 'isabel', 'jeff', 'klaus', 'leon', 'mark',
+           'noel']
+    partial_arr = arr[:5]
+    tree = MerkleTree(partial_arr)
+    print(tree.get_root_hash())
 
-    print("\nLeaves dictionary:")
-    for key, value in tree.node_dict.items():
-        print("\t%s : %s" % (key, value))
-
-    print("\nChildren dictionary:")
-    for key, value in tree.child_nodes_dict.items():
-        print("\t%s : %s" % (key, value))
-    
-    test_1 = 'brian'
-    test_1_hash = hashlib.sha256(test_1.encode('utf-8')).hexdigest()  # returns : f3fe5a51a2be8c6dc715028858fcba82ee021be7687e4f95b45086b8ffb1a23f
-
-    test_2 = 'alice'
-    test_2_hash = hashlib.sha256(test_2.encode('utf-8')).hexdigest()  # returns : 2bd806c97f0e00af1a1fc3328fa763a9269723c8db8fac4f93af71db186d6e90
-    
-    print("\n\nInclusion test no.1\n\t- name : %s : %s" % (test_1, tree.check_inclusion(test_1)))  # returns : False
-    print("\t- hash : %s : %s" % (test_1_hash, tree.check_inclusion(test_1_hash)))  # returns : False
-    print("\nInclusion test no.2\n\t- name : %s : %s" % (test_2, tree.check_inclusion(test_2)))  # returns : True
-    print("\t- hash : %s : %s" % (test_2_hash, tree.check_inclusion(test_2_hash)))  # returns : True
-
-    # TODO: add argument parser to the main.py when all functions are completed
-
+    # TODO: write appropriate inclusion and consistency check functions, comment the code and make tests
 
 if __name__ == '__main__':
     main()
--- merkle_tree.py
+++ merkle_tree.py
@@ -1,81 +1,62 @@
-from aux import *
+from hashlib import sha256
 
 
 class Node:
-    def __init__(self, value, name):
-        self.left_parent = None
-        self.right_parent = None
-        self.name = name
-        self.value = value
-        self.hash_value = get_hash_value(value)
+    def __init__(self, _left_child, _right_child, _name, _value):
+        self.parent = None
+        self.left_child = _left_child
+        self.right_child = _right_child
+        self.name = _name
+        self.value = _value
+        self.hash_value = sha256(self.value.encode('utf-8')).hexdigest()
 
 
 class MerkleTree:
-    def __init__(self, arr):
-        self.leaves = arr
-        self.node_dict = dict()
-        self.child_nodes_dict = dict()
+    def __init__(self, lst):
+        self.nodes = list()
+        tmp = list()
+        for elem in lst:
+            node = Node(None, None, elem, elem)
+            self.nodes.insert(-len(self.nodes), node)
+            tmp.append(node)
+        self.structure = self.build_tree(tmp)
 
-        self.final_hash = None
-
-    # def build_tree(self):
-        k = 0
-        nodes = []
-        for leaf in self.leaves:
-            node = Node(leaf, leaf)
-            nodes.append(node)
-            self.node_dict[node.name] = node.hash_value
-
-        for i in range(len(nodes)):
-            tmp_array = []
-            for j in range(0, len(nodes), 2):
-                k += 1
-                parent1 = nodes[j]
-                if j+1 < len(nodes):
-                    parent2 = nodes[j+1]
-                else:
-                    tmp_array.append(parent1)
-                    break
-
-                concatenated_hash = parent1.hash_value + parent2.hash_value
-                concatenated_name = parent1.name + "|" + parent2.name
-                child_node = Node(concatenated_hash, concatenated_name)
-                self.child_nodes_dict[child_node.name] = child_node.hash_value
-
-                tmp_array.append(child_node)
-            nodes = tmp_array
-        self.final_hash = nodes[0].hash_value
-
-    def get_final_hash(self):
-        return self.final_hash
-
-    def check_inclusion(self, elem):
-        if len(elem) == 64 and int(elem, 16):
-            if elem in self.node_dict.values():
-                return True
-            else:
-                return False
+    def create_parent(self, left_child, right_child):
+        if right_child is not None:
+            concatenated_name = left_child.name + "|" + right_child.name
+            concatenated_value = left_child.hash_value + right_child.hash_value
+            parent = Node(left_child, right_child, concatenated_name, concatenated_value)
+            left_child.parent, right_child.parent = parent, parent
         else:
-            if elem in self.node_dict.keys():
-                return True
+            concatenated_name = left_child.name
+            concatenated_value = left_child.value
+            parent = Node(None, None, concatenated_name, concatenated_value)
+            left_child.parent = parent
+
+        flag = False
+        for elem in self.nodes:
+            if elem.name == parent.name:
+                self.nodes.remove(elem)
+                self.nodes.insert(0, parent)
+                flag = True
+        if not flag:
+            self.nodes.insert(0, parent)
+
+        return parent
+
+    def build_tree(self, lst):
+        if len(lst) == 1:
+            return lst[0]
+        parents = []
+        for i in range(0, len(lst), 2):
+            left_child = lst[i]
+            if i + 1 < len(lst):
+                right_child = lst[i+1]
             else:
-                return False
+                parents.append(self.create_parent(left_child, None))
+                break
+            parents.append(self.create_parent(left_child, right_child))
+        return self.build_tree(parents)
 
-
-def check_consistency(tree1, tree2):  # tree1 - subtree, tree2 - maintree
-    # create temporary trees and swap trees, if first given tree is not the smallest
-    if len(tree1.leaves) > len(tree2.leaves):
-        tree, subtree = tree1, tree2
-    else:
-        subtree, tree = tree1, tree2
-
-    # check if tree1 is subset of tree2
-    for i in range(len(subtree.leaves)):
-        if subtree.leaves[i] is not tree.leaves[i]:
-            return False
-
-    tree_childs = tree.child_nodes_dict
-    tree_left_parents = extract_parents(tree.node_dict, 0)
-    tree_right_parents = extract_parents(tree.node_dict, 1)
-
-    # TODO: write consistency proof function, because it would be MUCH easier than revere engineering droid76's solution
+    def get_root_hash(self):
+        return self.structure.hash_value
