ac609523e89ac647e4616856917475400e6e92e1
Alexander Malcew <amalcew@protonmail.com>
2021-12-21 18:30:41 +0100

    Comments added, check_inclusion() method added

--- merkle_tree.py
+++ merkle_tree.py
@@ -2,6 +2,7 @@ from hashlib import sha256
 
 
 class Node:
+    """ Stores crucial data of the parent node, children nodes, name and values """
     def __init__(self, _left_child, _right_child, _name, _value):
         self.parent = None
         self.left_child = _left_child
@@ -12,51 +13,71 @@ class Node:
 
 
 class MerkleTree:
+    """ Tree structure storing leaves, branches and root hashes """
     def __init__(self, lst):
         self.nodes = list()
         tmp = list()
         for elem in lst:
             node = Node(None, None, elem, elem)
-            self.nodes.insert(-len(self.nodes), node)
-            tmp.append(node)
+            self.nodes.insert(-len(self.nodes), node)  # inserts node at the beginning to maintain proper indexing
+            tmp.append(node)  # temporary list used in build_tree() function
         self.structure = self.build_tree(tmp)
 
     def create_parent(self, left_child, right_child):
-        if right_child is not None:
+        """ Creates the parent node from given children and updates fields of children and parent """
+        if right_child is not None:  # case 1: parent has two children
             concatenated_name = left_child.name + "|" + right_child.name
             concatenated_value = left_child.hash_value + right_child.hash_value
-            parent = Node(left_child, right_child, concatenated_name, concatenated_value)
-            left_child.parent, right_child.parent = parent, parent
-        else:
+            parent = Node(left_child, right_child, concatenated_name, concatenated_value)  # create parent node
+            left_child.parent, right_child.parent = parent, parent  # update children' parent field
+        else:  # case 2: parent has one child
             concatenated_name = left_child.name
             concatenated_value = left_child.value
-            parent = Node(None, None, concatenated_name, concatenated_value)
-            left_child.parent = parent
+            parent = Node(None, None, concatenated_name, concatenated_value)  # create parent node
+            left_child.parent = parent  # update child's parent field
 
         flag = False
-        for elem in self.nodes:
+        for elem in self.nodes:  # loop checking the occurrence of parent on the nodes list
             if elem.name == parent.name:
+                # check if parent isn't already on the list. If it is, remove it and append a new one
                 self.nodes.remove(elem)
                 self.nodes.insert(0, parent)
                 flag = True
-        if not flag:
+
+        if not flag:  # if no duplicates were found, insert the parent node at the beginning of the list
             self.nodes.insert(0, parent)
 
         return parent
 
     def build_tree(self, lst):
+        """ Recursive method generating tree structure from given list of leaves """
         if len(lst) == 1:
             return lst[0]
+
         parents = []
-        for i in range(0, len(lst), 2):
+        for i in range(0, len(lst), 2):  # loop iterating through next levels of the tree
             left_child = lst[i]
-            if i + 1 < len(lst):
+            if i + 1 < len(lst):  # if there is two children, continue
                 right_child = lst[i+1]
-            else:
+            else:  # if there is only one child, append it to parents list and break the loop
                 parents.append(self.create_parent(left_child, None))
                 break
             parents.append(self.create_parent(left_child, right_child))
         return self.build_tree(parents)
 
     def get_root_hash(self):
+        """ Returns the root (final) hash """
         return self.structure.hash_value
+
+    def check_inclusion(self, elem):
+        """ Method searching for presence of passed parameter in the Merkle tree """
+        if len(elem) == 64 and int(elem, 16):  # passed parameter is a hash
+            for i in range(len(self.nodes)):
+                if elem == self.nodes[i].hash_value:
+                    return True
+            return False
+        else:  # passed parameter is a string
+            for i in range(len(self.nodes)):
+                if elem == self.nodes[i].name:
+                    return True
+            return False
