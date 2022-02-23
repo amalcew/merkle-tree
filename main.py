6895df06ef50190a9230e17fb5773d44a1552bdf
Aleksander Malcew <amalcew@protonmail.com>
2022-02-23 23:20:56 +0100

    extracted methods from original project

--- main.py
+++ main.py
@@ -1,46 +1,11 @@
 from merkle_tree import MerkleTree
-from hashlib import sha256
-
-
-def inclusion_test(tree):
-    test1 = 'brian'
-    test1_hash = sha256(test1.encode('utf-8')).hexdigest()  # returns : f3fe5a51a2be8c6dc715028858fcba82ee021be7687e4f95b45086b8ffb1a23f
-
-    test2 = 'alice'
-    test2_hash = sha256(test2.encode('utf-8')).hexdigest()  # returns : 2bd806c97f0e00af1a1fc3328fa763a9269723c8db8fac4f93af71db186d6e90
-
-    test3 = 'alice|bob'
-
-    test4 = 'alice|carol'
-
-    test5 = 'alice|bob|carol|david'
-
-    test6 = 'alice|david|bob|carol'
-
-    print("Inclusion test no.1\n\t- name : %s : %s" % (test2, tree.check_inclusion(test2)))     # returns : True
-    print("\t- hash : %s : %s" % (test2_hash, tree.check_inclusion(test2_hash)))                # returns : True
-    print("\nInclusion test no.2\n\t- name : %s : %s" % (test1, tree.check_inclusion(test1)))   # returns : False
-    print("\t- hash : %s : %s" % (test1_hash, tree.check_inclusion(test1_hash)))                # returns : False
-    print("\nInclusion test no.3\n\t- name : %s : %s" % (test3, tree.check_inclusion(test3)))   # returns : True
-    print("\nInclusion test no.4\n\t- name : %s : %s" % (test4, tree.check_inclusion(test4)))   # returns : False
-    print("\nInclusion test no.5\n\t- name : %s : %s" % (test5, tree.check_inclusion(test5)))   # returns : True
-    print("\nInclusion test no.6\n\t- name : %s : %s" % (test6, tree.check_inclusion(test6)))   # returns : False
 
 
 def main():
-    # arr = ['alice', 'bob', 'carol', 'david', 'eve', 'fred', 'george', 'hans', 'isabel', 'jeff', 'klaus', 'leon', 'mark', 'noel']
-    arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    partial_arr = arr[:12]
-    tree = MerkleTree(partial_arr)
-    # print("Root hash: %s\n" % (tree.get_root_hash()))
-    # inclusion_test(tree)
-    # tree.show(engine='graphviz')
-    tree.dfs()
-
-    # TODO:
-    #  1) debug create_parent() method for inappropriate parent-child bound in some cases i.e. for tree with 6 leaves
-    #     (hashes are still correct for some reason, yay...)
-    #   2) rewrite check_inclusion() method to work with a inorder dfs
+    arr = ['alice', 'bob', 'carol', 'david', 'eve', 'fred', 'george', 'hans', 'isabel', 'jeff', 'klaus', 'leon', 'mark',
+           'noel']
+    tree = MerkleTree(arr)
+    print("Root hash: %s\n" % (tree.get_root_hash()))
 
 
 if __name__ == '__main__':
--- merkle_tree.py
+++ merkle_tree.py
@@ -1,13 +1,19 @@
-from hashlib import sha256
 import treelib
+import json
+import os
+import platform
+from hashlib import sha256
 from graphviz import Source
 from os import remove
 from datetime import datetime
 from time import sleep
 
+now = datetime.now().strftime("%Y%m%d%H%M%S")
+
 
 class Node:
     """ Stores crucial data of the parent node, children nodes, name and values """
+
     def __init__(self, _left_child, _right_child, _name, _value):
         self.parent = None
         self.left_child = _left_child
@@ -19,83 +25,172 @@ class Node:
 
 class MerkleTree:
     """ Tree structure storing leaves, branches and root hashes """
+
     def __init__(self, lst):
-        self.nodes = list()
-        tmp = list()
+        self.nodes_dict = dict()
+        self.nodes_list = list()
+        self.leaves_list = list()
+
+        nodes = list()  # temporary list used in build_tree() function
         for elem in lst:
             node = Node(None, None, elem, elem)
-            self.nodes.insert(-len(self.nodes), node)  # inserts node at the beginning to maintain proper indexing
-            tmp.append(node)  # temporary list used in build_tree() function
-        self.structure = self.build_tree(tmp)
+            nodes.append(node)
+            self.leaves_list.append(node.name)
+        self.structure = self._build_tree(nodes)
+        self._generate_data_structures()  # generate nodes_dict and nodes_list
 
-    def create_parent(self, left_child, right_child):
+    @staticmethod
+    def _create_parent(_left_child, _right_child):
         """ Creates the parent node from given children and updates fields of children and parent """
-        if right_child is not None:  # case 1: parent has two children
-            concatenated_name = left_child.name + "|" + right_child.name
-            concatenated_value = left_child.hash_value + right_child.hash_value
-            parent = Node(left_child, right_child, concatenated_name, concatenated_value)  # create parent node
-            left_child.parent, right_child.parent = parent, parent  # update children' parent field
+
+        if _right_child is not None:  # case 1: parent has two children
+            concatenated_name = _left_child.name + "|" + _right_child.name
+            concatenated_value = _left_child.hash_value + _right_child.hash_value
+            parent = Node(_left_child, _right_child, concatenated_name,
+                          concatenated_value)  # create parent node
+            _left_child.parent, _right_child.parent = parent, parent  # update children' parent field
         else:  # case 2: parent has one child
-            concatenated_name = left_child.name
-            concatenated_value = left_child.value
+            concatenated_name = _left_child.name
+            concatenated_value = _left_child.value
             parent = Node(None, None, concatenated_name, concatenated_value)  # create parent node
-            left_child.parent = parent  # update child's parent field
-            if left_child.left_child:
+            _left_child.parent = parent  # update child's parent field
+            if _left_child.left_child:
                 # verify if parent node have children
                 # (correct inappropriate parent-children bound in specific cases of trees)
-                parent.left_child = left_child.left_child
-                parent.right_child = left_child.right_child
-
-        flag = False
-        for elem in self.nodes:  # loop checking the occurrence of parent on the nodes list
-            if elem.name == parent.name:
-                # check if parent isn't already on the list. If it is, remove it and append a new one
-                self.nodes.remove(elem)
-                self.nodes.insert(0, parent)
-                flag = True
-
-        if not flag:  # if no duplicates were found, insert the parent node at the beginning of the list
-            self.nodes.insert(0, parent)
+                parent.left_child = _left_child.left_child
+                parent.right_child = _left_child.right_child
 
         return parent
 
-    def build_tree(self, lst):
+    def _build_tree(self, _lst):
         """ Recursive method generating tree structure from given list of leaves """
-        if len(lst) == 1:
-            return lst[0]
+
+        if len(_lst) == 1:
+            return _lst[0]
 
         parents = []
-        for i in range(0, len(lst), 2):  # loop iterating through next levels of the tree
-            left_child = lst[i]
-            if i + 1 < len(lst):  # if there is two children, continue
-                right_child = lst[i+1]
+        for i in range(0, len(_lst), 2):  # loop iterating through next levels of the tree
+            left_child = _lst[i]
+            if i + 1 < len(_lst):  # if there is two children, continue
+                right_child = _lst[i + 1]
             else:  # if there is only one child, append it to parents list and break the loop
-                parents.append(self.create_parent(left_child, None))
+                parents.append(self._create_parent(left_child, None))
                 break
-            parents.append(self.create_parent(left_child, right_child))
-        return self.build_tree(parents)
+            parents.append(self._create_parent(left_child, right_child))
+        return self._build_tree(parents)
+
+    def _generate_data_structures(self):
+        """ Generates nodes dictionary with tree structure, used by json methods """
+
+        # TODO: optimise the tree not to generate useless node dictionaries,
+        #  which are later removed from the main dict
+
+        def add(_node):
+            """ Add node to the dictionary """
+
+            # create nested dictionaries for node's children, if present
+            tmp_left_child = self.nodes_dict[_node.left_child.name] if _node.left_child is not None else None
+            tmp_right_child = self.nodes_dict[_node.right_child.name] if _node.right_child is not None else None
+            # add node dictionary with children data
+            self.nodes_dict[_node.name] = {'name': _node.name,
+                                           'value': _node.value,
+                                           'hash_value': _node.hash_value,
+                                           'left_child': tmp_left_child,
+                                           'right_child': tmp_right_child}
+            if _node not in self.nodes_list:  # add the node to the list, which is used by show() method
+                self.nodes_list.insert(0, _node)
+
+        def generator(_node):
+            """ Recursive method that add node parameters as dictionary to nodes dictionary """
+
+            if _node.left_child:  # traverse through left subtree
+                generator(_node.left_child)
+            else:  # recursion base case
+                add(_node)
+                return
+
+            if _node.right_child:  # traverse through right subtree
+                generator(_node.right_child)
+            else:  # recursion base case
+                add(_node)
+                return
+
+            add(_node)
+
+        generator(self.structure)  # generate the nodes_dict
+
+        # overwrite the dictionary to store only root with its structure
+        self.nodes_dict = self.nodes_dict[self.structure.name]
 
     def get_root_hash(self):
         """ Returns the root (final) hash """
+
         return self.structure.hash_value
 
     def check_inclusion(self, elem):
-        """ Method searching for presence of passed parameter in the Merkle tree """
-        if len(elem) == 64 and int(elem, 16):  # passed parameter is a hash
-            for i in range(len(self.nodes)):
-                if elem == self.nodes[i].hash_value:
-                    return True
-            return False
-        else:  # passed parameter is a string
-            for i in range(len(self.nodes)):
-                if elem == self.nodes[i].name:
-                    return True
-            return False
+        """ Check if passed node is in the tree """
+
+        # TODO: rewrite the function to use the dfs, not the loop
+        for node in self.nodes_list:
+            if elem == node.name or elem == node.hash_value:
+                return True
+        return False
+
+    def check_consistency(self, subtree):
+        """ Check if passed subtree is part of main tree"""
+
+        # TODO: rewrite the function to generate consistency proof
+        for i in range(len(subtree.leaves_list)):
+            main_leaf = self.leaves_list[i]
+            sub_leaf = subtree.leaves_list[i]
+            if main_leaf != sub_leaf:
+                return False
+        return True
+
+    def dump(self):
+        """ Dumps the nodes dictionary to .json file """
+
+        tree_name = "tree-%s.json" % now
+        file = open(tree_name, "w")
+        json.dump(self.nodes_, file, indent=6)  # dump to json
+        file.close()
+
+    def show(self, engine='treelib', save=False):
+        """ Method printing tree structure, using treelib library """
+
+        tree = treelib.Tree()
+        for node in self.nodes_list:
+            if node.name == self.structure.name:
+                tree.create_node(node.name, node.name)  # root node
+            else:
+                tree.create_node(node.name, node.name, parent=node.parent.name)  # branch/leaf node
+
+        if engine == 'graphviz':  # use graphviz frontend to generate the visualization
+            tree_name = "tree-%s" % now
+            tree.to_graphviz(filename=tree_name, shape='record')
+            source = Source.from_file(tree_name)
+
+            if platform in ['linux', 'linux2']:                      # linux
+                if os.fork():  # parent process
+                    os.wait()  # wait for child process to finish
+                    if not save:
+                        remove(tree_name + ".pdf")
+                    remove(tree_name)
+                else:  # child process
+                    source.view()
+                    sleep(1)
+            else:                                                     # windows
+                source.view()
+                remove(tree_name)
+        else:  # use treelib frontend to generate the visualization
+            tree.show()
+            if save:
+                tree.save2file("tree-%s" % now)
 
     def dfs(self):
-        def inorder(struct):
+        def _dfs(struct):
             if struct.left_child is not None:
-                inorder(struct.left_child)
+                _dfs(struct.left_child)
 
             if struct.parent is not None:
                 print(struct.name)
@@ -103,30 +198,6 @@ class MerkleTree:
                 print(struct.name)
 
             if struct.right_child is not None:
-                inorder(struct.right_child)
-        inorder(self.structure)
+                _dfs(struct.right_child)
 
-    def show(self, engine='treelib', save=False):
-        """ Method printing tree structure, using treelib library """
-        tree = treelib.Tree()
-        for node in self.nodes:
-            if node.name == self.structure.name:
-                tree.create_node(node.name, node.name)  # root node
-            else:
-                tree.create_node(node.name, node.name, parent=node.parent.name)  # branch/leaf node
-
-        if engine == 'graphviz':  # use graphviz frontend to generate the visualization
-            now = datetime.now().strftime("%Y%m%d%H%M%S")
-            tree_name = "tree-%s" % now
-            tree.to_graphviz(filename=tree_name, shape='record')
-            source = Source.from_file(tree_name)
-            source.view()
-            sleep(0.3)
-            if not save:
-                remove(tree_name + ".pdf")
-            remove(tree_name)
-        else:  # use treelib frontend to generate the visualization
-            tree.show()
-            if save:
-                now = datetime.now().strftime("%Y%m%d%H%M%S")
-                tree.save2file("tree-%s" % now)
+        _dfs(self.structure)
